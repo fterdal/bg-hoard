96514f8cc66f3dd916929106b9c258584aee80b1
Finn Terdal <finnterdal@gmail.com>
2021-07-06 10:58:54 -0500

    create new nx repo and first component

---
+++ apps/store/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    MatCardModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
