62c73c08c35f7174efd664cefd0c999882385f9a
Finn Terdal <finnterdal@gmail.com>
2021-07-07 10:46:03 -0500

    adds dependency constraints

--- libs/api/util-notifications/src/lib/api-util-notifications.ts
+++ libs/api/util-notifications/src/lib/api-util-notifications.ts
@@ -1,4 +1,4 @@
-import { StoreFeatureGameDetailModule } from '@bg-hoard/store/feature-game-detail';
+// import { StoreFeatureGameDetailModule } from '@bg-hoard/store/feature-game-detail';
 
 export function sendNotification(clientId: string) {
   console.log('sending notification to client: ', clientId);
