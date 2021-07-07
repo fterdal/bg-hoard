102a02f51946bbe1d8334b7d81a1d66a9d891be7
Finn Terdal <finnterdal@gmail.com>
2021-07-07 11:14:17 -0500

    adds schema for custom generator

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,8 +1,17 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
+interface Schema {
+  name: string;
+  directory: 'store' | 'api' | 'shared';
+}
+
 export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
+  await libraryGenerator(host, {
+    name: 'util-' + schema.name,
+    directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`
+  });
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
