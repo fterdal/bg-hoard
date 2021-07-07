dbca9029dc037308309e7747e4c5598f0f2e6dbb
Finn Terdal <finnterdal@gmail.com>
2021-07-07 10:58:32 -0500

    generates generator

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,17 +1,8 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-interface Schema {
-  name: string;
-  directory: 'store' | 'api' | 'shared';
-}
-
-export default async function (host: Tree, schema: Schema) {
-  await libraryGenerator(host, {
-    name: `util-${schema.name}`,
-    directory: schema.directory,
-    tags: `type:util, scope:${schema.directory}`,
-  });
+export default async function (host: Tree, schema: any) {
+  await libraryGenerator(host, { name: schema.name });
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
